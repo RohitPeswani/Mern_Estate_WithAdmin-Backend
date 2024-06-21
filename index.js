5994faf2baae86bafb4be24a76c9896619093540
Rohit Programmer <rohitpeswani35@gmail.com>
2024-06-21 09:35:19 +0530

    solved cors error

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ mongoose
 
 const app = express();
 
-// app.use(cors({
-//   origin: 'http://localhost:5173',
-// }))
+app.use(cors({
+  origin: 'http://localhost:5173',
+}))
 app.use(express.json());
 
 app.use(cookieParser());
